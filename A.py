e16edcd0b33fa3b8e97ae37bbdf58aaa1f38dc19
KedaloEM <kedaloem@yandex.ru>
2015-10-09 22:45:28 +0300

    Please enter the commit message for your changes. Lines starting

--- A.py
+++ A.py
@@ -3,8 +3,7 @@ output=open('output.txt','w')
 A=[]
 B=intinput.readlines()
 N=int(B[0])
-for i in range(N):
-    A=A+list(map(int, B[i].split()))
+A=A+list(map(int, B[1].split()))
 b=0
 for elem in A:
     if A.count(elem)==2:
